049c7de6e9a6144202bb780a438e65937a6060d3
Chai Phonbopit <phonbopit@gmail.com>
2023-08-07 21:55:16 +0700

    rename path to product

--- intro-to-react-part-3/src/router.tsx
+++ intro-to-react-part-3/src/router.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         element: <About />,
       },
       {
-        path: 'dashboard',
+        path: 'product',
         element: <Product />,
       },
       {
